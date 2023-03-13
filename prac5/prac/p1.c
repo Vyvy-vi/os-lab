edad4cb04127d7277d3ef9658568f45a4afb21c0
user <user@kali.lpu.com>
2023-03-13 07:25:28 -0400

    feat: update prac4 for kali support

--- prac5/prac/p1.c
+++ prac5/prac/p1.c
@@ -1,1 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]) {
+	int n = 1;
+	if (argc > 1) {
+		n = argv[1];
+	}
+	fork();
+	printf("%d\n", n);
+
+}
